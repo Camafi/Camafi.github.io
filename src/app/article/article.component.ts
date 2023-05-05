cbac0693004fc84076fb1a1a2619fb56b603f26e
Bertonneau CLément <bertonneauclemen@gmail.com>
2023-05-05 14:41:20 +0200

    ajout de modal pour test les articles

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  isVisible : boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+  
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
+}
